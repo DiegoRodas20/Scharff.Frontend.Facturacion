d73f8ba9f50f735cc7d29f41eeebc367ee3757df
David Rosales Maldonado <ddmaldonado@stefanini.com>
2022-12-17 01:19:41 -0500

    implementacion de vista main/client

--- src/app/presentation/admin/client/manage-client/manage-client.component.ts
+++ src/app/presentation/admin/client/manage-client/manage-client.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-manage-client',
-    templateUrl: './manage-client.component.html'
+    templateUrl: './manage-client.component.html',
+    styleUrls: [ './manage-client.component.scss']
 })
-
 export class ManageClientComponent implements OnInit {
 
     constructor() { }
